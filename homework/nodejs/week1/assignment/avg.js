09a7950d7231a1bcb93d636b310039a4d340746b
Payal Rattan <payalrattan21@gmail.com>
2025-06-03 10:17:31 +0200

    update avg.js according to the feedback

--- homework/nodejs/week1/assignment/avg.js
+++ homework/nodejs/week1/assignment/avg.js
@@ -1,21 +1,18 @@
 const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log("Please provide at least one number.");
+  process.exit(1);
+}
 let sum = 0;
-let count = 0;
 
-for (let arg of args) {
-  let num = Number(arg);
+for (const arg of args) {
+  const num = Number(arg);
   if (isNaN(num)) {
     console.log("Please provide only numeric values.");
     process.exit(1);
   }
   sum += num;
-  count++;
 }
 
-if (count === 0) {
-  console.log("Please provide at least one number.");
-  process.exit(1);
-}
-else{
-console.log(sum / count);
-}
+const average = sum / args.length;
+console.log(average);

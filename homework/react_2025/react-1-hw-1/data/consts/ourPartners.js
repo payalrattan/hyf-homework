ad2d969d595f3a61187475648b09f066ca67d5d9
Payal Rattan <payalrattan21@gmail.com>
2025-06-21 13:56:56 +0200

    Week 1 task for React " About Us" page completed!!!!!

---
+++ homework/react_2025/react-1-hw-1/data/consts/ourPartners.js
@@ -0,0 +1,47 @@
+export const our_Partners= {
+  overview:
+    "We collaborate with some of the most respected names in the space and technology industries to make every journey extraordinary.",
+ partner_Names: [
+    {
+      id: 1,
+      alt: "Alphabet",
+      logo: "/business_partners/alphabet-logo.png",
+    },
+    {
+      id: 2,
+      alt: "Amazon",
+      logo: "/business_partners/amazon_logo.png",
+    },
+    {
+      id: 3,
+      alt: "CBC",
+      logo: "/business_partners/CBC_Logo_White.png",
+    },
+    
+    {
+      id: 4,
+      alt: "NYU",
+      logo: "/business_partners/nyu-logo.png",
+    },
+    {
+      id: 5,
+      alt: "Microsoft",
+      logo: "/business_partners/Microsoft-Logo-white.png",
+    },
+    {
+      id: 6,
+      alt: "Queens University",
+      logo: "/business_partners/QueensLogo_white.png",
+    },
+    {
+      id: 7,
+      alt: "Samsung",
+      logo: "/business_partners/samsung-logo.png",
+    },
+    {
+      id: 8,
+      alt: "Sodexo",
+      logo: "/business_partners/sodexo-logo.png",
+    },
+  ],
+};
